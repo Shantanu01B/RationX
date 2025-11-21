90c5fad46d5676ed16bb911a409f5af6a1ff54f0
Shantanu <shantanubarge2005@gmail.com>
2025-11-21 23:49:04 +0530

    Updated API config

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify';
 
 // 1. Create the connection to your Backend
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
     headers: {
         'Content-Type': 'application/json'
     }
